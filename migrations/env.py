bfb0697a9b44bb2ecdb19e6e35960f677db902d0
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-07 12:34:45 +0300

    Update config import path

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from app.models.models import metadata as model_metadata
 
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from app.core.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
