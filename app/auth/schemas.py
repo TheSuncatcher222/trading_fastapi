2618a4a73423c6f649e107a158b600c2f4586365
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-07 15:02:29 +0300

    Add user: create, read, update

---
+++ app/auth/schemas.py
@@ -0,0 +1,33 @@
+from typing import Optional, Generic
+
+from fastapi_users import schemas, models
+from pydantic import EmailStr, ConfigDict
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: models.ID
+    email: EmailStr
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+    role_id: int
+    name_first: str
+    name_second: str
+    username: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(schemas.CreateUpdateDictModel, Generic[models.ID]):
+    email: EmailStr
+    password: str
+    name_first: Optional[str]
+    name_second: Optional[str]
+    username: str
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    email: EmailStr
+    name_first: Optional[str]
+    name_second: Optional[str]
+    username: str
