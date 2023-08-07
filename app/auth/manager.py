69741d24c4c72ebe67d15bf8aefd072476b46cc2
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-07 18:10:39 +0300

    Add user_dict['is_active'] = True

--- app/auth/manager.py
+++ app/auth/manager.py
@@ -64,6 +64,8 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         user_dict['hashed_password'] = self.password_helper.hash(password)
         # Update original func with 1 row below:
         user_dict['role_id'] = ROLE_ID_DEFAULT
+        # ToDo: produce verifications step in on_after_register adn delete:
+        user_dict['is_active'] = True
         created_user = await self.user_db.create(user_dict)
 
         await self.on_after_register(created_user, request)
